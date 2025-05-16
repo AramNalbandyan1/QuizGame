ba1a0be772354cd42439c27f4cd7a9ab9939e6f4
AramNalbandyan1 <aramnalband2216@gmail.com>
2025-05-16 21:46:44 +0400

    Version 1.0.0

--- app/src/main/java/com/example/quizgame/ResultActivity.java
+++ app/src/main/java/com/example/quizgame/ResultActivity.java
@@ -2,6 +2,8 @@ package com.example.quizgame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,15 +17,20 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         TextView resultText = findViewById(R.id.resultText);
+        TextView leftEmoji = findViewById(R.id.leftEmoji);
+        TextView rightEmoji = findViewById(R.id.rightEmoji);
         Button goHomeButton = findViewById(R.id.goHomeButton);
 
-        // Получаем данные из Intent
         int correctAnswers = getIntent().getIntExtra("correctAnswers", 0);
         int totalQuestions = getIntent().getIntExtra("totalQuestions", 10);
         String totalTime = getIntent().getStringExtra("totalTime");
 
         resultText.setText("Правильных ответов: " + correctAnswers + " из " + totalQuestions + "\n" +
                 "Время: " + totalTime);
+
+        Animation bounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        leftEmoji.startAnimation(bounce);
+        rightEmoji.startAnimation(bounce);
 
         goHomeButton.setOnClickListener(v -> {
             startActivity(new Intent(ResultActivity.this, MainActivity.class));
